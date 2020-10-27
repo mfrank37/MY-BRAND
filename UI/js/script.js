15fb45620a55ee77e8495bcf3e5f860204509b90
MUNGERI Frank <54407341+mfrank37@users.noreply.github.com>
2020-10-27 16:05:21 +0200

    feature: landing page (#5)
    * feat(landing page): create landing page
        - create landing page
        - add navigation
        - add name and profession
        - add about
        - add skills
        - add projects
        - add experience
        - add contacts
        - add blog
    
    * feat(landing page): layout and style the landing page
        - add new css file css/main.css
        - add css styling from css/main.css
    
    * feat(landing page): add images
        - add background image
        - add profile picture
       -  add images for skills, projects and experiences.
        
    * feat(landing page): make responsive
        - make landing page responsive

---
+++ UI/js/script.js
@@ -0,0 +1,9 @@
+/* Navbar control */
+function toggleNav() {
+  let x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
