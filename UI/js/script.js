216cbf71bb6f80495cfe1b646e6d385e41d43f99
MUNGERI Frank <54407341+mfrank37@users.noreply.github.com>
2020-10-29 09:44:26 +0200

    feature: view all articles page (#8)
        - create page to view articles
        - add admin's navigation
        - layout, style and make responsive
        - create popup for deleting article

--- UI/js/script.js
+++ UI/js/script.js
@@ -7,3 +7,36 @@ function toggleNav() {
     x.className = "topnav";
   }
 }
+
+function cancelDeleteArticle(){
+  let deleteSection = document.querySelector('.confirm-delete-section');
+  deleteSection.classList.add('hide');
+}
+
+
+function confirmDeletePopup({ target }){
+  let deleteSection = document.querySelector('.confirm-delete-section');
+  
+  // embed the clicked article in the delete popup
+  let titlePlaceholder = deleteSection.querySelector('h2');
+  titlePlaceholder.innerHTML = target.parentNode.parentNode.querySelector('.article-text h4').innerHTML;
+
+  // add id to the big main btn
+  deleteSection.querySelector('button.big-delete-btn').id = target.id;
+  console.log(deleteSection.querySelector('button.big-delete-btn'));
+  // make visible
+  deleteSection.classList.remove('hide');
+}
+
+
+// Gets run when loaded.
+function start(){
+
+  let deleteButtons = document.querySelectorAll('.article .btns .delete');
+  deleteButtons.forEach( button =>{
+    button.addEventListener('click', confirmDeletePopup);
+  });
+
+}
+// starts here
+start();
