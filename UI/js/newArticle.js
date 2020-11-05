27e5bb894fc5ad9119cc60263b73bc8a64fbe58e
Mungeri Frank <mungerifrank@gmail.com>
2020-11-05 13:34:04 +0200

    fx(new article): fix indent to 2

--- UI/js/newArticle.js
+++ UI/js/newArticle.js
@@ -4,43 +4,43 @@ coverImage = document.querySelector("#coverImage");
 uploader = document.querySelector("#progress");
 
 coverImage.addEventListener('change', evt => {
-    uploader.value = 0;
-    // image
-    file = evt.target.files[0];
-    // storage ref
-    let storageRef = STORAGE.ref('articles/' + file.name );
-    let task = storageRef.put(file);
-    
-    task.on('state_changed',
+  uploader.value = 0;
+  // image
+  file = evt.target.files[0];
+  // storage ref
+  let storageRef = STORAGE.ref('articles/' + file.name);
+  let task = storageRef.put(file);
+
+  task.on('state_changed',
     function progress(snapshot) {
-        console.log(snapshot.bytesTransferred/snapshot.totalBytes * 100, " %  - uploaded Article's cover image");
-        uploader.value = snapshot.bytesTransferred/snapshot.totalBytes * 100;
+      console.log(snapshot.bytesTransferred / snapshot.totalBytes * 100, " %  - uploaded Article's cover image");
+      uploader.value = snapshot.bytesTransferred / snapshot.totalBytes * 100;
     },
     function error(err) {
-        console.log('Upload failed! ' + err);
+      console.log('Upload failed! ' + err);
     },
-    function complete(){
-        // get image reference
-        console.log('Completed upload ' + file.name );        
+    function complete() {
+      // get image reference
+      console.log('Completed upload ' + file.name);
     });
 })
 
 form.addEventListener('submit', evt => {
-    evt.preventDefault();
-    let title = form.querySelector('#article-title');
-    let description = form.querySelector('#article-body');
-    // add article to firestore articles collection
-    FIRESTORE.collection('articles').add({
-        title: title.value,
-        coverImage: file.name,
-        description: description.value.split(/\n{2,}/),
-        time: new Date(),
-        comments: {}
-    }).then( () => {
-        uploader.value = 0;
-        title.value = '';
-        coverImage.value = '';
-        description.value = '';
-        console.log('Article uploaded');
-    });
+  evt.preventDefault();
+  let title = form.querySelector('#article-title');
+  let description = form.querySelector('#article-body');
+  // add article to firestore articles collection
+  FIRESTORE.collection('articles').add({
+    title: title.value,
+    coverImage: file.name,
+    description: description.value.split(/\n{2,}/),
+    time: new Date(),
+    comments: {}
+  }).then(() => {
+    uploader.value = 0;
+    title.value = '';
+    coverImage.value = '';
+    description.value = '';
+    console.log('Article uploaded');
+  });
 });
